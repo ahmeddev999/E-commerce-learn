6ead528f0991bdb6588ab6b2146d366f3906304a
Ahmed <ahmad45salim45@gmail.com>
2026-06-17 23:57:33 +0300

    zustand setup

---
+++ frontend/src/stores/useUserStore.js
@@ -0,0 +1,7 @@
+import { create } from 'zustand';
+import { axiosInstance as axios } from '../lib/axios';
+import { toast } from 'react-hot-toast';
+
+const useUserStore = create((set, get) => ({
+
+}));
